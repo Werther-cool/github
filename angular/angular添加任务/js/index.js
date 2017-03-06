65922fc43cb59f8559e84566c66fd0260892c6d9
Werther <zhouwangyulu@icloud.com>
2017-03-06 09:06:21 +0800

    angular-2

--- angular/angular添加任务/js/index.js
+++ angular/angular添加任务/js/index.js
@@ -1,12 +1,10 @@
-
-
-$(function () {
+$(function() {
 
     /*1.获取当前导航条距离窗口的y值*/
     var nav_top = $('.nav').offset().top;
 
     /*2.监听窗口的滚动*/
-    $(window).on('scroll',function () {
+    $(window).on('scroll', function() {
 
         /*2.1 获取当前滚动的top方向偏移量*/
         var scroll_top = $(window).scrollTop();
@@ -16,25 +14,29 @@ $(function () {
 
             /*2.2.1 把导航的定位设置为fixed*/
             $('.nav').css({
-                'position':'fixed',
-                'top':0,
-                'box-shadow':'0 1px 3px rgba(0, 0, 0, .3)'
+                'position': 'fixed',
+                'top': 0,
+                'box-shadow': '0 1px 3px rgba(0, 0, 0, .3)'
             });
 
             /*2.2.2 把图片的透明度改为1,让其显示*/
-            $('.nav img').css({opacity:1});
+            $('.nav img').css({
+                opacity: 1
+            });
 
-        }else {
+        } else {
             /*2.2.3 把导航的定位设置为absolute  */
             $('.nav').css({
-                'position':'absolute',
-                'top':nav_top,
-                'border-bottom':'none',
-                'box-shadow':'none'
+                'position': 'absolute',
+                'top': nav_top,
+                'border-bottom': 'none',
+                'box-shadow': 'none'
             });
-            
+
             /*2.2.4 把图片的透明度改为0,让其变透明*/
-            $('.nav img').css({opacity:0});
+            $('.nav img').css({
+                opacity: 0
+            });
         }
 
     });
@@ -43,7 +45,7 @@ $(function () {
      * 二.处理tab标签切换
      * */
     /*1.监听tab点击*/
-    $('.header li').click(function () {
+    $('.header li').click(function() {
         $(this).addClass('cur').siblings().removeClass('cur');
         /*2.切换内容*/
         /*2.1 获取当前点击的角标*/
@@ -54,47 +56,44 @@ $(function () {
     });
 
     /*
-    * 三.处理返回顶部逻辑
-    * */
-     /*1.指定一个值*/
+     * 三.处理返回顶部逻辑
+     * */
+    /*1.指定一个值*/
     var ori_num = 100;
-     /*2.监听窗口滚动*/
-    $(window).on('scroll',function () {
+    /*2.监听窗口滚动*/
+    $(window).on('scroll', function() {
         /*2.1 如果当前滚动值大于窗口的高度, 让其显示否则隐藏*/
-        var scroll_top  = $(window).scrollTop();
+        var scroll_top = $(window).scrollTop();
         console.log(scroll_top);
         if (scroll_top > ori_num) {
-            $('.back_top').stop().fadeTo(200,1);
-        }else {
-            $('.back_top').stop().fadeTo(200,0);
+            $('.back_top').stop().fadeTo(200, 1);
+        } else {
+            $('.back_top').stop().fadeTo(200, 0);
         }
 
     });
 
-     /*3.监听返回顶部点击*/
-    $('.back_top').click(function () {
+    /*3.监听返回顶部点击*/
+    $('.back_top').click(function() {
         /*3.1 让窗口滚动到顶部*/
-        $('html body').animate({scrollTop:0});
+        $('html body').animate({
+            scrollTop: 0
+        });
     });
 });
 
-/*1.创建模型*/
-var app = angular.module('app',[]);
+var app = angular.module('app', []);
+app.controller('WerController', ['$scope', function($scope) {
 
-/*2.创建控制器*/
-app.controller('taskController',['$scope',function ($scope) {
+    $scope.itemList = []
+    $scope.submit = function() {
 
-    /*定义数组保存添加的任务*/
-    $scope.taskList = [];
-    /*6.提交按钮点击*/
-    $scope.submit = function () {
-      var taskItem = {
-          name:$scope.task,
-          check:false
-      }
-      $scope.taskList.push(taskItem);
-      $scope.task = '';
-    };
-}]);
+        var item = {
+            name: $scope.task
+        }
+        $scope.itemList.push(item);
+        item = '';
+    }
+}])
 /*3.到视图当中绑定模块*/
 /*4.到视图当中绑定控制器*/
