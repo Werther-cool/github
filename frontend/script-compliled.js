88937bb412672d0c19b83e1c3770dabeb775ab02
Werther <zhouwangyulu@icloud.com>
2017-03-19 17:53:06 +0800

    todos

--- frontend/script-compliled.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var breakfast = function breakfast(dessert, drink) {
-  return dessert + drink;
-};
--- frontend/script.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-let breakfast = (dessert, drink) => dessert + drink;
-
-asd
